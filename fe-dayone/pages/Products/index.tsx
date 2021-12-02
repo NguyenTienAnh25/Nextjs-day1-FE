ccd6ce256e20657f3b243426db58877dbc19af1f
Nguyễn Tiến Anh <anhtien2k00@gmail.com>
2021-12-02 08:52:58 +0700

    update

---
+++ fe-dayone/pages/Products/index.tsx
@@ -0,0 +1,39 @@
+import {InferGetStaticPropsType} from 'next'
+
+type Product = {
+    id: number,
+    name:String,
+    email: String,
+    address: String,
+    color: String
+}
+export const getStaticProps = async()=>{
+    require('dotenv').config()
+    const resapi = await fetch(`https://nextjs-day1.vercel.app/api`);
+    const posts:Product[] = await resapi.json()
+    return {
+        props: {
+            posts,
+        },
+        revalidate: 10,
+      }
+}
+function Product({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+    const data: Product[] = posts;
+    return (
+        <ul>
+        {posts.map((post) => (
+        <li style={{
+            listStyle: "none",
+        }}>
+            <h3>{post.id}</h3>
+            <p>{post.name}</p>
+            <p>{post.email}</p>
+            <p>{post.address}</p>
+            <p>{post.color}</p>
+        </li>
+         ))}
+        </ul>
+    )
+}
+export default Product
